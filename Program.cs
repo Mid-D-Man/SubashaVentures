8d9b0e3eef235c4d1d2ce7448f10fdbac8cae449
Abdulhamid Manman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2025-10-05 18:29:51 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SubashaVentures;
-
+using SubashaVentures.Services.Navigayion;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddSingleton<INavigationService, NavigationService>();
 await builder.Build().RunAsync();
