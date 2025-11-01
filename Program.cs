9bdd403dc970a87f3a2ae290b3dfe7ff2fb16785
Abdulhamid Manman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2025-11-01 01:48:50 +0100

    Update Program.cs --publish

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// ==================== HTTP CLIENT ====================
+// ==================== HTTP CLIENT =================== =
 builder.Services.AddScoped(sp => new HttpClient 
 {  
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
