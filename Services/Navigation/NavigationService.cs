67c55fcf11f3d0ff2bab12fbb0b5fb66fa7f670b
Abdulhamid Manman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2025-12-24 14:59:08 +0100

    Update NavigationService.cs

--- Services/Navigation/NavigationService.cs
+++ Services/Navigation/NavigationService.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.WebUtilities;
 
 namespace SubashaVentures.Services.Navigation;
 
@@ -7,7 +6,6 @@ public class NavigationService : INavigationService
 {
     private readonly NavigationManager _navigationManager;
     private bool _isSidePanelOpen;
-    private string _searchQuery = string.Empty;
     
     public NavigationService(NavigationManager navigationManager)
     {
@@ -27,22 +25,7 @@ public class NavigationService : INavigationService
         }
     }
     
-    public string SearchQuery
-    {
-        get => _searchQuery;
-        private set
-        {
-            if (_searchQuery != value)
-            {
-                _searchQuery = value;
-                SearchQueryChanged?.Invoke(this, value);
-            }
-        }
-    }
-
-    
     public event EventHandler<bool>? SidePanelStateChanged;
-    public event EventHandler<string>? SearchQueryChanged;
     public event EventHandler? FilterPanelToggleRequested;
     public event EventHandler? FiltersChanged;
     
@@ -50,17 +33,8 @@ public class NavigationService : INavigationService
     public void CloseSidePanel() => IsSidePanelOpen = false;
     public void ToggleSidePanel() => IsSidePanelOpen = !IsSidePanelOpen;
     public void SetSidePanelState(bool isOpen) => IsSidePanelOpen = isOpen;
-    public void UpdateSearchQuery(string query) => SearchQuery = query ?? string.Empty;
-    public void ClearSearchQuery() => SearchQuery = string.Empty;
     public void ToggleFilterPanel() => FilterPanelToggleRequested?.Invoke(this, EventArgs.Empty);
     public void NotifyFiltersChanged() => FiltersChanged?.Invoke(this, EventArgs.Empty);
-    
-    public string? GetQueryParameter(string key)
-    {
-        var uri = new Uri(_navigationManager.Uri);
-        var queryParams = QueryHelpers.ParseQuery(uri.Query);
-        return queryParams.TryGetValue(key, out var value) ? value.ToString() : null;
-    }
     
     public void NavigateTo(string uri)
     {
