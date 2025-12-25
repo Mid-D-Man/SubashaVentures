c6ca5ab81396301c1ec9bb4f79ada98c42ca4d43
Abdulhamid Manman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2025-12-25 15:47:49 +0100

    Delete Models/Auth directory

--- Models/Auth/AuthError.cs
+++
@@ -1,9 +0,0 @@
-// Models/Auth/AuthError.cs
-namespace SubashaVentures.Models.Auth;
-
-public class AuthError
-{
-    public int code { get; set; }
-    public string error_code { get; set; } = string.Empty;
-    public string msg { get; set; } = string.Empty;
-}
