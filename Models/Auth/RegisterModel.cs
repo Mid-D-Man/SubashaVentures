0ceadeddea7c8667cbf2a1b2e3cbe2af45c45b8d
Abdulhamid Manman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2025-12-25 15:34:13 +0100

    Create RegisterModel.cs

---
+++ Models/Auth/RegisterModel.cs
@@ -0,0 +1,11 @@
+// Models/Auth/RegisterModel.cs
+namespace SubashaVentures.Models.Auth;
+
+public class RegisterModel
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string? PhoneNumber { get; set; }
+}
